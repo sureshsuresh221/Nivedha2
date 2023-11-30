d0586e26d3be97508b042cf7e3374bf232b09fbe
sureshkuruba <sureshkuruba641@gamail.com>
2023-11-30 09:58:02 +0000

    solution

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,52 @@
+// Write your JS code here
+import {Component} from 'react'
+
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+class BlogsItem extends Component {
+  state = {isLoading: true, blogsData: []}
+
+  componentDidMount() {
+    this.getBlogsData()
+  }
+
+  getBlogsData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+
+    const data = await response.json()
+
+    const updatedData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatar_url,
+      author: eachItem.author,
+      topic: eachItem.topic,
+    }))
+    this.setState({blogsData: updatedData, isLoading: false})
+  }
+
+  render() {
+    const {blogsData, isLoading} = this.state
+
+    return (
+      <div className="blog-list-container">
+        {isLoading ? (
+          <Loader type="TailSpinner" color="green" height={50} width={50} />
+        ) : (
+          blogsData.map(item => (
+            <BlogItem blogsItemDetails={item} key={item.id} />
+          ))
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogsItem
